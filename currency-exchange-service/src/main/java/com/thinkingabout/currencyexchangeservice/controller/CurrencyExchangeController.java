54a9cc7c16398ce0fcde76984503e1ccfaa54789
emitpool <canalp3rry@gmail.com>
2022-05-27 15:12:07 -0300

    Add new things

--- currency-exchange-service/src/main/java/com/thinkingabout/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/thinkingabout/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package com.thinkingabout.currencyexchangeservice.controller;
 
 import com.thinkingabout.currencyexchangeservice.model.CurrencyExchange;
+import com.thinkingabout.currencyexchangeservice.repository.CurrencyExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +16,21 @@ public class CurrencyExchangeController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private CurrencyExchangeRepository currencyExchangeRepository;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
 
+//        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
+
+        if(currencyExchange == null){
+            throw new RuntimeException("Unable to Find data for " + from + " to " + to);
+        }
         String port = environment.getProperty("local.server.port");
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
         currencyExchange.setEnvironment(port);
+
         return currencyExchange;
 
 
