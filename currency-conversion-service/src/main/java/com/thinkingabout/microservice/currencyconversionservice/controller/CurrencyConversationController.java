a373fe73eb3a16bfbd37257b37e84e1450061314
emitpool <canalp3rry@gmail.com>
2022-06-01 08:28:59 -0300

    Add API GATEWAY

--- currency-conversion-service/src/main/java/com/thinkingabout/microservice/currencyconversionservice/controller/CurrencyConversationController.java
+++ currency-conversion-service/src/main/java/com/thinkingabout/microservice/currencyconversionservice/controller/CurrencyConversationController.java
@@ -1,7 +1,7 @@
 package com.thinkingabout.microservice.currencyconversionservice.controller;
 
-import com.thinkingabout.microservice.currencyconversionservice.CurrencyExchangeProxy;
 import com.thinkingabout.microservice.currencyconversionservice.model.CurrencyConversion;
+import com.thinkingabout.microservice.currencyconversionservice.proxy.CurrencyExchangeProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
--- currency-conversion-service/src/main/java/com/thinkingabout/microservice/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/thinkingabout/microservice/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -1,4 +1,4 @@
-package com.thinkingabout.microservice.currencyconversionservice;
+package com.thinkingabout.microservice.currencyconversionservice.proxy;
 
 import com.thinkingabout.microservice.currencyconversionservice.model.CurrencyConversion;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.math.BigDecimal;
 
-@FeignClient(name="currency-exchange", url="localhost:8000")
+//@FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
